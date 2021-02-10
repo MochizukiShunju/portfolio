54eef278609e71a7cb48c10d30898ecb4b48ddc8
望月隼寿 <mochidzukishunju@mochidzukishunkotobukinoMacBook-Pro.local>
2021-02-10 19:55:22 +0900

    [start]

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,1 +1,9 @@
-alert('Hello World');
+$(function (){
+  $('.box1').slideDown(function (){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp();
+  });
+});
